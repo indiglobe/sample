214f28c3e925b9cad4a7a604e916199e49fd5d71
Nirvik Purkait <nirvikpurkait@gmail.com>
2026-02-12 17:12:29 +0530

    netlify added

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import viteTsConfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
+import netlify from "@netlify/vite-plugin-tanstack-start";
 
 const config = defineConfig({
   plugins: [
@@ -19,6 +20,8 @@ const config = defineConfig({
     tailwindcss(),
 
     tanstackStart(),
+
+    netlify(),
 
     viteReact({
       babel: {
